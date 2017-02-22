4dfc491a2f7ac09d9e987b72e20c12af8d9962bb
Andreas Brommund <andreas@brommund.se>
2017-02-22 16:25:31 +0100

    Start to write code for the lexer.

--- src/main/scala/punkt0/lexer/Lexer.scala
+++ src/main/scala/punkt0/lexer/Lexer.scala
@@ -12,14 +12,46 @@ object Lexer extends Phase[File, Iterator[Token]] {
 
     // TODO: implement this method
 
+    var current = source.next()
+    var currentToken : Token
+
     new Iterator[Token] {
 
       def hasNext = {
-        ???
+        currentToken.kind,
       }
 
       def next = {
-        ???
+        if(current != EOF){
+          new Token(EOF)
+        }
+        var nextChar = source.next()
+        current match {
+          case digit if digit.isDigit => //Digit
+          case alfa if alfa.isLetter  => //Keyword or ID
+          case '"' => //String literal
+          case ':' => new Token(COLON)
+          case ';' => new Token(SEMICOLON)
+          case '.' => new Token(DOT)
+          case ',' => new Token(COMMA)
+          case '=' => // == or =
+          case '!' => new Token(BANG)
+          case '(' => new Token(LPAREN)
+          case ')' => new Token(RPAREN)
+          case '{' => new Token(LBRACE)
+          case '}' => new Token(RBRACE)
+          case '&' => //&&
+          case '|' => //||
+          case '<' => new Token(LESSTHAN)
+          case '+' => new Token(PLUS)
+          case '-' => new Token(MINUS)
+          case '*' => new Token(TIMES)
+          case '/' => // '/' '//' '/*'
+          case _   => new Token(BAD) //consume char or have multiple bads until valid char
+        }
+        //Current = nextChar
+        //return match funk
+
       }
     }
   }
