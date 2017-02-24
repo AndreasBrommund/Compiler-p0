e1a2c9ee06ef7f0941db5ed8311535af82c2d294
Andreas Brommund <andreas@brommund.se>
2017-02-24 11:48:33 +0100

    Run the most of the tokens

--- src/main/scala/punkt0/lexer/Lexer.scala
+++ src/main/scala/punkt0/lexer/Lexer.scala
@@ -7,52 +7,132 @@ import java.io.File
 object Lexer extends Phase[File, Iterator[Token]] {
   import Reporter._
 
+  var lastSeenToken : Option[Token] = None
+  var previousIsWhitespace = false
+  var nextChar : Char = ' '
+
   def run(f: File)(ctx: Context): Iterator[Token] = {
     val source = scala.io.Source.fromFile(f)
 
     // TODO: implement this method
 
-    var current = source.next()
-    var currentToken : Token
+    var currentChar = source.next()
 
     new Iterator[Token] {
 
       def hasNext = {
-        currentToken.kind,
+        ???
       }
 
       def next = {
-        if(current != EOF){
-          new Token(EOF)
-        }
-        var nextChar = source.next()
-        current match {
-          case digit if digit.isDigit => //Digit
-          case alfa if alfa.isLetter  => //Keyword or ID
-          case '"' => //String literal
-          case ':' => new Token(COLON)
-          case ';' => new Token(SEMICOLON)
-          case '.' => new Token(DOT)
-          case ',' => new Token(COMMA)
-          case '=' => // == or =
-          case '!' => new Token(BANG)
-          case '(' => new Token(LPAREN)
-          case ')' => new Token(RPAREN)
-          case '{' => new Token(LBRACE)
-          case '}' => new Token(RBRACE)
-          case '&' => //&&
-          case '|' => //||
-          case '<' => new Token(LESSTHAN)
-          case '+' => new Token(PLUS)
-          case '-' => new Token(MINUS)
-          case '*' => new Token(TIMES)
-          case '/' => // '/' '//' '/*'
-          case _   => new Token(BAD) //consume char or have multiple bads until valid char
-        }
-        //Current = nextChar
-        //return match funk
+
+        //TODO fix EOF
+
+        nextChar = source.next()
+
+        var res = a(currentChar,source)
+
+        currentChar = nextChar
+        res
 
       }
+    }
+  }
+  /*def validStart() : Boolean = lastSeenToken match {
+      case Some(t) => previousIsWhitespace || !(t.isInstanceOf[ID] || t.isInstanceOf[INTLIT] || t.isInstanceOf[STRLIT])
+      case None => true
+    }*/
+  def getToken(literal: String) : Token = literal match {
+    case "object" => new Token(OBJECT)
+    case "class" => new Token(CLASS)
+    case "def" => new Token(DEF)
+    case "override" => new Token(OVERRIDE)
+    case "var" => new Token(VAR)
+    case "Unit" => new Token(UNIT)
+    case "String" => new Token(STRING)
+    case "extends" => new Token(EXTENDS)
+    case "Int" => new Token(INT)
+    case "Boolean" => new Token(BOOLEAN)
+    case "while" => new Token(WHILE)
+    case "if" => new Token(IF)
+    case "else" => new Token(ELSE)
+    case "true" => new Token(TRUE)
+    case "false" => new Token(FALSE)
+    case "this" => new Token(THIS)
+    case "null" => new Token(NULL)
+    case "new" => new Token(NEW)
+    case "println" => new Token(PRINTLN)
+    case _ => new ID(literal)
+  }
+  //TODO better name needed
+  def a (currentChar: Char, source: scala.io.BufferedSource) : Token =  {
+    currentChar match {
+      case digit if digit.isDigit && digit != '0' =>
+
+        var integer = digit.asDigit
+        while (nextChar.isDigit){
+          integer = integer*10+nextChar.asDigit
+          nextChar = source.next()
+        }
+
+        new INTLIT(integer)
+
+      case alfa if alfa.isLetter  =>
+        val buffer = new StringBuilder
+        buffer.append(alfa)
+
+        while(nextChar.isLetter || nextChar.isDigit || nextChar == '_'){
+          buffer.append(nextChar)
+          nextChar = source.next()
+        }
+
+        getToken(buffer.toString())
+
+      case whiteSpace if(whiteSpace.isWhitespace) => ??? //TODO White space
+
+      case '"' =>
+        var buffer = new StringBuilder
+        while(nextChar != '"'){
+          buffer.append(nextChar)
+          if(!source.hasNext)
+            new Token(BAD) //TODO fix later
+          nextChar = source.next()
+        }
+        new STRLIT(buffer.toString())
+
+      case ':' => new Token(COLON)
+      case ';' => new Token(SEMICOLON)
+      case '.' => new Token(DOT)
+      case ',' => new Token(COMMA)
+      case '=' =>
+        if(nextChar == '=')
+          new Token(EQUALS)
+        else
+          new Token(EQSIGN)
+
+      case '!' => new Token(BANG)
+      case '(' => new Token(LPAREN)
+      case ')' => new Token(RPAREN)
+      case '{' => new Token(LBRACE)
+      case '}' => new Token(RBRACE)
+      case '&' =>
+        if(nextChar == '&')
+          new Token(AND)
+        else
+          new Token(BAD) //TODO fix later
+
+      case '|' =>
+        if(nextChar == '|')
+          new Token(OR)
+        else
+          new Token(BAD) //TODO fix later
+
+      case '<' => new Token(LESSTHAN)
+      case '+' => new Token(PLUS)
+      case '-' => new Token(MINUS)
+      case '*' => new Token(TIMES)
+      case '/' => ??? //TODO // '/' '//' '/*'
+      case _   => new Token(BAD) //TODO fix consume char or have multiple bads until valid char
     }
   }
 }
