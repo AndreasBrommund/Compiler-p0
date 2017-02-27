81ea046da92a8edd3ac9665d19a998b31aa92d6d
Andreas Brommund <andreas@brommund.se>
2017-02-27 23:32:58 +0100

    Comments are working I think

--- src/main/scala/punkt0/lexer/Lexer.scala
+++ src/main/scala/punkt0/lexer/Lexer.scala
@@ -7,47 +7,33 @@ import punkt0.Positioned
 
 
 object Lexer extends Phase[File, Iterator[Token]] {
+
+
+
   import Reporter._
 
   def run(f: File)(ctx: Context): Iterator[Token] = {
     val source = scala.io.Source.fromFile(f)
 
-    var currentChar : Option[Char] = Some(source.next()) //TODO breaks if the file is empty
+    var currentChar: Option[Char] = Some(source.next()) //TODO breaks if the file is empty
+
 
     new Iterator[Token] {
-
       var hasNext = true
 
       def next = {
-
-        val res = consumeUnecessaryChars(currentChar,getNextChar(source),source)
-        currentChar = res._1
-        val nextChar = res._2
-
-        (currentChar,nextChar) match {
-          case(Some(_),Some(_)) =>
-          case (Some(_),None)   =>
-          case (None,None) =>
-        }
-
-        //TODO Need to think about how BAD works, if we gonna have pos here.
-
-
-        if(currentChar.isEmpty){
-          hasNext = false
-          new Token(EOF).setPos(pos)
-        }else{
-          val tuple = findToken(currentChar.get,nextChar,source)
-          currentChar = tuple._2
-          tuple._1.setPos(pos)
-        }
+        val tuple = findToken(currentChar, f, source)
+        currentChar = tuple._2
+        hasNext = tuple._3
+        tuple._1
       }
     }
   }
+
   /*def validStart() : Boolean = lastSeenToken match {
-      case Some(t) => previousIsWhitespace || !(t.isInstanceOf[ID] || t.isInstanceOf[INTLIT] || t.isInstanceOf[STRLIT])
-      case None => true
-    }*/
+  case Some(t) => previousIsWhitespace || !(t.isInstanceOf[ID] || t.isInstanceOf[INTLIT] || t.isInstanceOf[STRLIT])
+  case None => true
+  }*/
   def getToken(literal: String): Token = literal match {
     case "object" => new Token(OBJECT)
     case "class" => new Token(CLASS)
@@ -73,41 +59,54 @@ object Lexer extends Phase[File, Iterator[Token]] {
 
   //Example: 123ABS is allowed in the Lexer but are gonna yield an error in the Parser
   //Return condition: nextChar is always gonna be the char after the token, e.q. 123A nextChar = A and token = 123
-  def findToken(currentChar: Char, next: Option[Char],source: scala.io.BufferedSource): (Token, Option[Char]) = {
+  def findToken(curr: Option[Char], f: File, source: scala.io.BufferedSource): (Token, Option[Char],Boolean,Positioned) = {
+    var currentChar = curr
+    var hasNext = true
 
-    var nextChar = next
+    while (currentChar.isDefined && currentChar.get.isWhitespace) {
+      currentChar = getNextChar(source)
+    }
 
-    val newToken = currentChar match {
-      case digit if digit.isDigit && digit != '0' =>
-        var integer = digit.asDigit
+    var pos = new Positioned {
+      setPos(f, source.pos)
+    }
 
-        while (nextChar.isDefined && nextChar.get.isDigit) {
-          integer = integer * 10 + nextChar.get.asDigit
-          nextChar = getNextChar(source)
-        }
+    if(currentChar.isEmpty) {
+      (new Token(EOF).setPos(pos),currentChar,false,pos)
+    }else {
+      var nextChar = getNextChar(source)
 
-        new INTLIT(integer)
+      val newToken = currentChar.get match {
+        case digit if digit.isDigit && digit != '0' =>
+          var integer = digit.asDigit
 
-      case alfa if alfa.isLetter =>
-        val buffer = new StringBuilder
-        buffer.append(alfa)
+          while (nextChar.isDefined && nextChar.get.isDigit) {
+            integer = integer * 10 + nextChar.get.asDigit
+            nextChar = getNextChar(source)
+          }
 
-        while (nextChar.isDefined && (nextChar.get.isLetterOrDigit || nextChar.get == '_')) {
-          buffer.append(nextChar.get)
-          nextChar = getNextChar(source)
-        }
+          new INTLIT(integer)
 
-        getToken(buffer.toString)
+        case alfa if alfa.isLetter =>
+          val buffer = new StringBuilder
+          buffer.append(alfa)
 
-      case '"' =>
-        val buffer = new StringBuilder
+          while (nextChar.isDefined && (nextChar.get.isLetterOrDigit || nextChar.get == '_')) {
+            buffer.append(nextChar.get)
+            nextChar = getNextChar(source)
+          }
 
-        while (nextChar.isDefined&&nextChar.get != '\n'&&nextChar.get != '"'){
-          buffer.append(nextChar.get)
-          nextChar = getNextChar(source)
-        }
+          getToken(buffer.toString)
 
-        nextChar match {
+        case '"' =>
+          val buffer = new StringBuilder
+
+          while (nextChar.isDefined && nextChar.get != '\n' && nextChar.get != '"') {
+            buffer.append(nextChar.get)
+            nextChar = getNextChar(source)
+          }
+
+          nextChar match {
             case Some('"') =>
               nextChar = getNextChar(source)
               new STRLIT(buffer.toString)
@@ -115,73 +114,95 @@ object Lexer extends Phase[File, Iterator[Token]] {
               nextChar = getNextChar(source)
               new Token(BAD) //TODO handle BAD better
 
-        }
+          }
 
-      case ':' => new Token(COLON)
-      case ';' => new Token(SEMICOLON)
-      case '.' => new Token(DOT)
-      case ',' => new Token(COMMA)
-      case '!' => new Token(BANG)
-      case '(' => new Token(LPAREN)
-      case ')' => new Token(RPAREN)
-      case '{' => new Token(LBRACE)
-      case '}' => new Token(RBRACE)
-      case '<' => new Token(LESSTHAN)
-      case '+' => new Token(PLUS)
-      case '-' => new Token(MINUS)
-      case '*' => new Token(TIMES)
-      case '/' => new Token(DIV)
-      case '&' =>
-        nextChar match {
-          case Some('&') =>
-            nextChar = getNextChar(source)
-            new Token(AND)
+        case ':' => new Token(COLON)
+        case ';' => new Token(SEMICOLON)
+        case '.' => new Token(DOT)
+        case ',' => new Token(COMMA)
+        case '!' => new Token(BANG)
+        case '(' => new Token(LPAREN)
+        case ')' => new Token(RPAREN)
+        case '{' => new Token(LBRACE)
+        case '}' => new Token(RBRACE)
+        case '<' => new Token(LESSTHAN)
+        case '+' => new Token(PLUS)
+        case '-' => new Token(MINUS)
+        case '*' => new Token(TIMES)
+        case '/' =>
+          nextChar match {
+            case Some('/') =>
+              nextChar = getNextChar(source)
+              while (nextChar.isDefined && nextChar.get != '\n') {
+                nextChar = getNextChar(source)
+              }
 
-          case _ => new Token(BAD) //TODO fix later
-        }
-      case '|' =>
-        nextChar match {
-          case Some('|') =>
-            nextChar = getNextChar(source)
-            new Token(OR)
+              nextChar match {
+                case Some(c) =>
+                  val r = findToken(Some(c), f, source)
+                  nextChar = r._2
+                  hasNext = r._3
+                  pos = r._4
+                  r._1
+                case None =>
+                  pos = new Positioned {
+                    setPos(f, source.pos)
+                  }
+                  hasNext = false
+                  new Token(EOF)
+              }
+            case Some('*') =>
+              nextChar = getNextChar(source)
+              var secondNext = getNextChar(source)
+              while (nextChar.isDefined&&secondNext.isDefined && !(nextChar.get == '*' && secondNext.get == '/')){
+                nextChar = secondNext
+                secondNext = getNextChar(source)
+              }
+              (nextChar,secondNext) match {
+                case (Some(_),Some(_)) =>
+                  val r = findToken(getNextChar(source), f, source)
+                  nextChar = r._2
+                  hasNext = r._3
+                  pos = r._4
+                  r._1
+                case _ =>
+                  nextChar = None
+                  new Token(BAD) //TODO fix later
+              }
 
-          case _ => new Token(BAD) //TODO fix later
-        }
-      case '=' =>
-        nextChar match {
-          case Some('=') =>
-            nextChar = getNextChar(source)
-            new Token(EQUALS)
+            case Some(_) | None => new Token(DIV)
+          }
+        case '&' =>
+          nextChar match {
+            case Some('&') =>
+              nextChar = getNextChar(source)
+              new Token(AND)
 
-          case _ => new Token(EQSIGN)
-        }
-      case _ => new Token(BAD) //TODO fix consume char or have multiple bads until valid char
+            case _ => new Token(BAD) //TODO fix later
+          }
+        case '|' =>
+          nextChar match {
+            case Some('|') =>
+              nextChar = getNextChar(source)
+              new Token(OR)
+
+            case _ => new Token(BAD) //TODO fix later
+          }
+        case '=' =>
+          nextChar match {
+            case Some('=') =>
+              nextChar = getNextChar(source)
+              new Token(EQUALS)
+
+            case _ => new Token(EQSIGN)
+          }
+        case _ => new Token(BAD) //TODO fix consume char or have multiple bads until valid char
+      }
+      (newToken.setPos(pos), nextChar,hasNext,pos)
     }
-
-    (newToken, nextChar)
   }
 
-  def consumeUnecessaryChars(currentChar: Option[Char],nextChar: Option[Char],source: scala.io.BufferedSource) : (Option[Char],Option[Char]) = {
-    var current = currentChar
-    var next = nextChar
-    var ret = false
 
-    (current,next) match {
-      case (Some('/'),Some('/')) =>
-        do{
-          current = getNextChar(source)
-        } while(current.isDefined && current.get != '\n')
-        next = getNextChar(source)
-      case (Some('/'),Some('*')) =>
-      case (Some(first),_) if first.isWhitespace =>
-      case _ => ret = true
-    }
-
-    if(!ret)
-      consumeUnecessaryChars(current,next,source)
-    else
-      (current,next)
-  }
 
   def getNextChar(source: scala.io.BufferedSource): Option[Char] = {
     if (source.hasNext)
