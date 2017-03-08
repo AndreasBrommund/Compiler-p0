20c47a347dfee3193e8d8c6a3df2d997a343866d
Andreas Brommund <andreas@brommund.se>
2017-03-08 17:32:53 +0100

    Implemented everything in the parser expect for expression

--- src/main/scala/punkt0/ast/Parser.scala
+++ src/main/scala/punkt0/ast/Parser.scala
@@ -44,56 +44,164 @@ object Parser extends Phase[Iterator[Token], Program] {
       while (currentToken.kind == CLASS) {
         classes += parseClassDeclaration
       }
-      new Program(parseMainDeclaration, classes.toList)
+      val mainDecl = parseMainDeclaration
+      eat(EOF)
+
+      new Program(mainDecl,classes.toList)
     }
 
     //ClassDeclaration ::=
     def parseClassDeclaration: ClassDecl = {
-      ???
+      eat(CLASS)
+      val className = parseIdent
+
+      //Extends
+      var extendsName : Option[Identifier] = None
+      if(currentToken.kind == EXTENDS){
+        eat(EXTENDS)
+        extendsName = Some(parseIdent)
+      }
+      eat(LBRACE)
+
+      //VarDeclaration
+      val vars = parseVarDeclarationBlock
+
+      //MethodDeclaration
+      var methods = new ListBuffer[MethodDecl]
+      while (currentToken.kind == OVERRIDE || currentToken.kind == DEF)
+        methods += parseMethodDeclaration
+
+      eat(RBRACE)
+      new ClassDecl(className,extendsName,vars.toList,methods.toList)
     }
 
     //MainDeclaration ::=
-    def parseMainDeclaration: MainMethod = {
+    def parseMainDeclaration: MainDecl = {
       eat(OBJECT)
       val obj = parseIdent
       eat(EXTENDS)
       val parent = parseIdent
       eat(LBRACE)
 
-      val varDecls = new ListBuffer[VarDecl]
-      //TODO Add decls to the list
-      var exprs = new ListBuffer[ExprTree]
-      //TODO Add exprs to the list
+      val vars = parseVarDeclarationBlock
 
+      val exprs = parseExpressionBlock
 
-      val main = new MethodDecl(
-        false, new UnitType, new Identifier("Main"), List(),
-        varDecls.toList, exprs.init.toList, exprs.last)
       eat(RBRACE)
-      new MainMethod(obj, parent, main)
+      new MainDecl(obj, parent, vars, exprs)
     }
 
     //VarDeclaration ::=
     def parseVarDeclaration: VarDecl = {
-      ???
+      eat(VAR)
+      val varName = parseIdent
+      eat(COLON)
+      val varType = parseType
+      eat(EQSIGN)
+      val varExpr = parseExpression
+      eat(SEMICOLON)
+      new VarDecl(varType,varName,varExpr)
+    }
+
+    def parseVarDeclarationBlock: List[VarDecl] = {
+      var vars = new ListBuffer[VarDecl]
+      while(currentToken.kind == VAR)
+        vars += parseVarDeclaration
+      vars.toList
+    }
+
+    //MethodDeclaration ::=
+    def parseMethodDeclaration: MethodDecl = {
+      var overrides = false
+
+      currentToken.kind match {
+        case OVERRIDE => overrides = true; readToken; eat(DEF)
+        case DEF => readToken
+        case _ => expected(OVERRIDE, DEF)
+      }
+
+
+      val methodName = parseIdent
+      eat(LPAREN)
+
+      var arguments = new ListBuffer[Formal]
+
+      //Get all method arguments
+      if (currentToken.kind == IDKIND) {
+        //Have arguments
+
+        def parseFormal = {
+          val argumentName = parseIdent
+          eat(COLON)
+          val argumentType = parseType
+          arguments += new Formal(argumentType, argumentName)
+        }
+
+        parseFormal //Get the first required argument
+        while (currentToken.kind == COMMA){
+          eat(COMMA)
+          parseFormal
+        }
+      }
+      eat(RPAREN)
+      eat(COLON)
+
+      //Get return type
+      val returnType = parseType
+
+      eat(EQSIGN)
+      eat(LBRACE)
+
+      val vars = parseVarDeclarationBlock
+
+      val exprs = parseExpressionBlock
+
+      eat(RBRACE)
+
+      new MethodDecl(overrides,returnType,methodName,
+        arguments.toList,vars,exprs.init,exprs.last)
+
     }
 
     //Type ::=
     def parseType: TypeTree = {
-      //TODO maybe read token
-      currentToken.kind match {
+      val typeTree = currentToken.kind match {
         case BOOLEAN => new BooleanType
         case INT => new IntType
         case STRING => new StringType
         case UNIT => new UnitType
         case _ => parseIdent
       }
+      readToken
+      typeTree
+    }
+
+    //Expression ::=
+    def parseExpression: ExprTree = {
+      //TODO FIX
+      val exprTree = currentToken.kind match {
+        case TRUE => new True
+        case FALSE => new False
+        case _ => expected(TRUE,FALSE) //TODO Fix
+      }
+      readToken
+      exprTree
+    }
+
+    def parseExpressionBlock : List[ExprTree] = {
+      var exprs = new ListBuffer[ExprTree]
+      exprs += parseExpression
+      while(currentToken.kind == SEMICOLON){
+        eat(SEMICOLON)
+        exprs += parseExpression
+      }
+      exprs.toList
     }
 
     //Identifier ::=
     def parseIdent: Identifier = currentToken match {
-      case id: ID => new Identifier(id.value)
-      case _ => expected(IDKIND)
+        case id: ID => readToken;new Identifier(id.value)
+        case _ => expected(IDKIND)
     }
 
     readToken
