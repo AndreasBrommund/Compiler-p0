ebabd2adf7051ce95c1bf6443d1d6e7697f1fd16
Andreas Brommund <andreas@brommund.se>
2017-03-11 19:02:57 +0100

    Update test script

--- runTest.sh
+++ runTest.sh
@@ -1,15 +1,58 @@
 #!/usr/bin/env bash
 
+#echo "Test valid lab2"
+#for f in testprograms/lab2/valid/*.p0
+#do
+#    CHECK=${f%.*}.check
+#    echo "$f -> $CHECK"
+#    sbt "run --tokens $f" > /dev/null 2> res.p0.ast
+#    DIFF=$(diff res.p0.ast $CHECK)
+#    crc32 res.p0.ast
+#    head -c 70 res.p0.ast
+#    echo ""
+#    if [ "$DIFF" != "" ]
+#    then
+
+#        echo "Fail $f"
+#        diff res.p0.ast $CHECK
+#        exit
+#    fi
+#done
+
+echo "Test invalid lab3"
+for f in testprograms/lab3/invalid/*.p0
+do
+    echo "$f"
+    sbt "run --ast $f" > /dev/null 2> res.p0.ast
+
+
+    if [ "$?" == "0" ]
+    then
+        echo "Fail $f"
+        exit
+    fi
+    crc32 res.p0.ast
+    head -c 100 res.p0.ast
+    echo ""
+done
+
+echo "Test valid lab3"
 for f in testprograms/lab3/valid/*.p0
 do
     AST=${f}.ast
-	echo "$f -> $AST"
-	sbt "run --ast $f" > /dev/null 2> res.p0.ast
-	DIFF=$(diff res.p0.ast $AST)
-	if [ "$DIFF" != "" ]
-	then
-	    echo "Fail $f"
-	    diff res.p0.ast $AST
-	    exit
-	fi
+    echo "$f -> $AST"
+    sbt "run --ast $f" > /dev/null 2> res.p0.ast
+    DIFF=$(diff res.p0.ast $AST)
+    crc32 res.p0.ast
+    head -c 90 res.p0.ast
+    echo ""
+    if [ "$DIFF" != "" ]
+    then
+
+        echo "Fail $f"
+        diff res.p0.ast $AST
+        exit
+    fi
 done
+
+
