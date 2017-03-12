0e03b91c95f84c5d98b14d7b01beff2ca5534da8
Andreas Brommund <andreas@brommund.se>
2017-03-12 15:20:35 +0100

    Implement pretty-printer

--- src/main/scala/punkt0/ast/Printer.scala
+++ src/main/scala/punkt0/ast/Printer.scala
@@ -27,7 +27,19 @@ object Printer {
           sb.append(";"); getPrettyString(n, sb)
         })
         sb.append("}")
-      case node: ClassDecl => ???
+      case node: ClassDecl =>
+        sb.append("class ")
+        getPrettyString(node.id,sb)
+        if(node.parent.isDefined){
+          sb.append(" extends ")
+          getPrettyString(node.parent.get,sb)
+        }
+        sb.append("{")
+        node.vars.foreach(n => getPrettyString(n, sb))
+        node.methods.foreach(n => {
+          sb.append(" "); getPrettyString(n, sb)
+        })
+        sb.append("}")
       case node: VarDecl =>
         sb.append("var ")
         getPrettyString(node.id, sb)
@@ -36,7 +48,31 @@ object Printer {
         sb.append("=")
         getPrettyString(node.expr, sb)
         sb.append(";")
-      case node: MethodDecl => ???
+      case node: MethodDecl =>
+        if(node.overrides){
+          sb.append("override ")
+        }
+        sb.append("def ")
+        getPrettyString(node.id,sb)
+        sb.append(" (")
+        if (node.args.nonEmpty) {
+          getPrettyString(node.args.head, sb)
+          node.args.tail.foreach(n => {sb.append(","); getPrettyString(n, sb)})
+        }
+        sb.append("):")
+        getPrettyString(node.retType,sb)
+        sb.append(" = {")
+        node.vars.foreach(n => getPrettyString(n, sb))
+        if(node.exprs.nonEmpty){
+          getPrettyString(node.exprs.head, sb)
+          node.exprs.tail.foreach(n => {sb.append(";"); getPrettyString(n, sb)})
+          sb.append(";")
+        }
+        getPrettyString(node.retExpr,sb)
+      case node: Formal =>
+        getPrettyString(node.id,sb)
+        sb.append(":")
+        getPrettyString(node.tpe,sb)
       case node: BooleanType => sb.append("Boolean")
       case node: IntType => sb.append("Int")
       case node: StringType => sb.append("String")
@@ -79,7 +115,16 @@ object Printer {
         sb.append("==")
         getPrettyString(node.rhs, sb)
         sb.append(")")
-      case node: MethodCall => ???
+      case node: MethodCall =>
+        getPrettyString(node.obj,sb)
+        sb.append(".")
+        getPrettyString(node.meth,sb)
+        sb.append("(")
+        if (node.args.nonEmpty) {
+          getPrettyString(node.args.head, sb)
+          node.args.tail.foreach(n => {sb.append(","); getPrettyString(n, sb)})
+        }
+        sb.append(")")
       case node: IntLit => sb.append(node.value)
       case node: StringLit => sb.append(node.value)
       case node: True => sb.append("true")
@@ -102,11 +147,33 @@ object Printer {
           node.exprs.tail.foreach(n => {sb.append(";"); getPrettyString(n, sb)})
         }
         sb.append("}")
-     case node: If => ???
-     case node: While => ???
-     case node: Println => ???
-     case node: Assign => ???
-     case _ => Reporter.fatal("Wring node type in tree",branch)
+      case node: If =>
+        sb.append("if (")
+        getPrettyString(node.expr,sb)
+        sb.append(")")
+        sb.append("{")
+        getPrettyString(node.thn,sb)
+        sb.append("}")
+        if(node.els.isDefined){
+          sb.append("else")
+          sb.append("{")
+          getPrettyString(node.els.get,sb)
+          sb.append("}")
+        }
+      case node: While =>
+        sb.append("while (")
+        getPrettyString(node.cond,sb)
+        sb.append(")")
+        getPrettyString(node.body,sb)
+      case node: Println =>
+        sb.append("println (")
+        getPrettyString(node.expr,sb)
+        sb.append(")")
+      case node: Assign =>
+        getPrettyString(node.id,sb)
+        sb.append("=")
+        getPrettyString(node.expr,sb)
+      case _ => Reporter.fatal("Wring node type in tree",branch)
     }
   }
 }
