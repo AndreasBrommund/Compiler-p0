b79852aa5f487f7cc22db8288ffe3e06dbea5f74
andreas <brommund@kth.se>
2017-05-09 11:48:25 +0200

    Update  runTest.sh

--- runTest.sh
+++ runTest.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+#wget http://www.csc.kth.se/~phaller/compilers/labs/punkt0_2.11-1.0.jar
+
 sbt compile
 
 echo "Test valid lab5"
@@ -10,12 +12,29 @@ do
 	rm -rf classes/
 	rm -rf classesOur/
 
+	#Reference compiler
 	scala -cp lib/cafebabe_2.11-1.2.jar:punkt0_2.11-1.0.jar punkt0.Main -d classes $f
+	
+	if [ "$?" != "0" ]
+    	then
+        	echo "This is not a valid program"
+        	exit
+	fi
+
 	java -cp classes Main > phaller.txt
 
+	#Our compiler
 	scala -cp "lib/cafebabe_2.11-1.2.jar:target/scala-2.11/classes/" punkt0.Main -d classesOur $f
+
+	if [ "$?" != "0" ]
+    	then
+        	echo "Fail $f"
+        	exit
+	fi	
+
 	java -cp classesOur Main > res.txt	
 
+	#Diff
 	diff res.txt phaller.txt
 	
 	crc32 res.txt	
@@ -27,7 +46,20 @@ do
 	echo $f
 
 	rm -rf sclassesOur/
+	rm -rf classes/
 
+	#Reference compiler
+
+	scala -cp lib/cafebabe_2.11-1.2.jar:punkt0_2.11-1.0.jar punkt0.Main -d classes $f
+
+	if [ "$?" != "1" ]
+    	then
+        	echo "This is not an unvalid program $f"
+        	exit
+	fi
+
+	#Our compiler
+	
 	scala -cp "lib/cafebabe_2.11-1.2.jar:target/scala-2.11/classes/" punkt0.Main -d classesOur $f
 	
 	if [ "$?" != "1" ]
